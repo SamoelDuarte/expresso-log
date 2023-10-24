c96516509677507ea62258a1f94812f8751b2ac5
SamoelDuarte <129750291+SamoelDuarte@users.noreply.github.com>
2023-10-24 11:01:24 -0300

    Update PedidosController.php

--- app/Http/Controllers/PedidosController.php
+++ app/Http/Controllers/PedidosController.php
@@ -233,7 +233,7 @@ class PedidosController extends Controller
                 } catch (RequestException $e) {
                     if($e->getResponse()->getReasonPhrase() == "Internal Server Error"){
                         Error::create(['erro' => 'Erro servidor interno Astralog'.$e->getMessage()]);
-                        exit;
+                       
                     }
                 }
 
