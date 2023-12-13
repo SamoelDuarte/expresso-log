a26c82871e0b173ee556c9e08fedd5ed777fe01d
SamoelDuarte <129750291+SamoelDuarte@users.noreply.github.com>
2023-12-13 10:45:50 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -231,7 +231,7 @@ Route::get('/updateStatusDBA', function () {
             $query->where('status', 'finalizado');
         })
         ->orderBy('id')
-        ->limit(10)
+        ->limit(7)
         ->get();
 
 
