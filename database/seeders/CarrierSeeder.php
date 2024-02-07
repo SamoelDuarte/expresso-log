37fa3d2abb5a0ed084439b01407d13f4f4f60e50
SamoelDuarte <129750291+SamoelDuarte@users.noreply.github.com>
2024-02-07 13:07:23 -0300

    Update CarrierSeeder.php

--- database/seeders/CarrierSeeder.php
+++ database/seeders/CarrierSeeder.php
@@ -13,106 +13,106 @@ class CarrierSeeder extends Seeder
      */
     public function run()
     {
-        $carrier = new Carrier([
-            'legal_name' => 'DBA EXPRESS LOGISTICS AND TRANSPORT LTD',
-            'trade_name' => 'DBA EXPRESS',
-            'state_registration' => 'EXEMPT',
-            'phone' => 'your_phone',
-            'rntrc' => 'your_rntrc',
-            'opted_for_simples_nacional' => false,
-            'zip_code' => '07430350',
-            'address' => 'AVENIDA TOWER AUTOMOTIVE, 701 - LARANJA AZEDA',
-            'number' => '701',
-            'complement' => 'your_complement',
-            'neighborhood' => 'LARANJA AZEDA',
-            'city' => 'ARUJÁ',
-            'state' => 'SP',
-            'landmark' => 'your_landmark',
-            'contact_name' => 'contact_name',
-            'contact_phone1' => 'contact_phone_1',
-            'contact_phone2' => 'contact_phone_2',
-            'contact_email' => 'contact_email',
-            'customer_service_phone' => 'customer_service_phone',
-            'customer_service_email' => 'customer_service_email',
-            'website' => 'company_website',
-            'title' => 'your_title',
-            'message' => 'your_message',
-        ]);
+        // $carrier = new Carrier([
+        //     'legal_name' => 'DBA EXPRESS LOGISTICS AND TRANSPORT LTD',
+        //     'trade_name' => 'DBA EXPRESS',
+        //     'state_registration' => 'EXEMPT',
+        //     'phone' => 'your_phone',
+        //     'rntrc' => 'your_rntrc',
+        //     'opted_for_simples_nacional' => false,
+        //     'zip_code' => '07430350',
+        //     'address' => 'AVENIDA TOWER AUTOMOTIVE, 701 - LARANJA AZEDA',
+        //     'number' => '701',
+        //     'complement' => 'your_complement',
+        //     'neighborhood' => 'LARANJA AZEDA',
+        //     'city' => 'ARUJÁ',
+        //     'state' => 'SP',
+        //     'landmark' => 'your_landmark',
+        //     'contact_name' => 'contact_name',
+        //     'contact_phone1' => 'contact_phone_1',
+        //     'contact_phone2' => 'contact_phone_2',
+        //     'contact_email' => 'contact_email',
+        //     'customer_service_phone' => 'customer_service_phone',
+        //     'customer_service_email' => 'customer_service_email',
+        //     'website' => 'company_website',
+        //     'title' => 'your_title',
+        //     'message' => 'your_message',
+        // ]);
 
-        $carrier->save();
+        // $carrier->save();
 
-        $document = new CarrierDocument();
-        $document->carrier_id = $carrier->id;
-        $document->number = "50160966000164";
-        $document->save();
+        // $document = new CarrierDocument();
+        // $document->carrier_id = $carrier->id;
+        // $document->number = "50160966000164";
+        // $document->save();
 
-        $document = new CarrierDocument();
-        $document->carrier_id = $carrier->id;
-        $document->number = "08982220000170";
-        $document->save();
+        // $document = new CarrierDocument();
+        // $document->carrier_id = $carrier->id;
+        // $document->number = "08982220000170";
+        // $document->save();
 
         // Repeat the same process for other carriers and documents.
 
-        $carrier1 = Carrier::create([
-            'legal_name' => 'MESH TRANSPORTES E LOGISTICA LTDA',
-            'trade_name' => 'MESH TRANSPORTES E LOGISTICA LTDA',
-            'state_registration' => 'EXEMPT',
-            'phone' => '47992912222',
-            'rntrc' => 'your_rntrc',
-            'opted_for_simples_nacional' => false,
-            'zip_code' => '06186260',
-            'address' => 'Avenida Leonil Crê Bortolosso',
-            'number' => '945',
-            'complement' => 'your_complement',
-            'neighborhood' => 'Quitaúna',
-            'city' => 'Osasco',
-            'state' => 'SP',
-            'landmark' => 'your_landmark',
-            'contact_name' => 'Mesh',
-            'contact_phone1' => '(47) 99291-2222',
-            'contact_phone2' => 'contact_phone_2',
-            'contact_email' => 'contact_email',
-            'customer_service_phone' => 'customer_service_phone',
-            'customer_service_email' => 'customer_service_email',
-            'website' => 'company_website',
-            'title' => 'your_title',
-            'message' => 'your_message',
-        ]);
+        // $carrier1 = Carrier::create([
+        //     'legal_name' => 'MESH TRANSPORTES E LOGISTICA LTDA',
+        //     'trade_name' => 'MESH TRANSPORTES E LOGISTICA LTDA',
+        //     'state_registration' => 'EXEMPT',
+        //     'phone' => '47992912222',
+        //     'rntrc' => 'your_rntrc',
+        //     'opted_for_simples_nacional' => false,
+        //     'zip_code' => '06186260',
+        //     'address' => 'Avenida Leonil Crê Bortolosso',
+        //     'number' => '945',
+        //     'complement' => 'your_complement',
+        //     'neighborhood' => 'Quitaúna',
+        //     'city' => 'Osasco',
+        //     'state' => 'SP',
+        //     'landmark' => 'your_landmark',
+        //     'contact_name' => 'Mesh',
+        //     'contact_phone1' => '(47) 99291-2222',
+        //     'contact_phone2' => 'contact_phone_2',
+        //     'contact_email' => 'contact_email',
+        //     'customer_service_phone' => 'customer_service_phone',
+        //     'customer_service_email' => 'customer_service_email',
+        //     'website' => 'company_website',
+        //     'title' => 'your_title',
+        //     'message' => 'your_message',
+        // ]);
         
-        $carrier1->documents()->create([
-            'number' => '37744796000105',
-        ]);
+        // $carrier1->documents()->create([
+        //     'number' => '37744796000105',
+        // ]);
 
-        $carrier3 = Carrier::create([
-            'legal_name' => 'ASTROLOG SERVICOS DE TRANSPORTE LTDA',
-            'trade_name' => 'ASTROLOG SERVICOS DE TRANSPORTE LTDA',
-            'state_registration' => 'EXEMPT',
-            'phone' => '4799291-2222',
-            'rntrc' => 'your_rntrc',
-            'opted_for_simples_nacional' => false,
-            'zip_code' => '04180075',
-            'address' => 'Travessa Ernesto Portante',
-            'number' => '55',
-            'complement' => 'your_complement',
-            'neighborhood' => 'Jardim Maria Estela',
-            'city' => 'São Paulo',
-            'state' => 'SP',
-            'landmark' => 'your_landmark',
-            'contact_name' => 'Astralog',
-            'contact_phone1' => '(47) 99291-2222',
-            'contact_phone2' => 'contact_phone_2',
-            'contact_email' => 'astrolog@fretebarato.com',
-            'customer_service_phone' => 'customer_service_phone',
-            'customer_service_email' => 'customer_service_email',
-            'website' => 'company_website',
-            'title' => 'your_title',
-            'message' => 'your_message',
-        ]);
+        // $carrier3 = Carrier::create([
+        //     'legal_name' => 'ASTROLOG SERVICOS DE TRANSPORTE LTDA',
+        //     'trade_name' => 'ASTROLOG SERVICOS DE TRANSPORTE LTDA',
+        //     'state_registration' => 'EXEMPT',
+        //     'phone' => '4799291-2222',
+        //     'rntrc' => 'your_rntrc',
+        //     'opted_for_simples_nacional' => false,
+        //     'zip_code' => '04180075',
+        //     'address' => 'Travessa Ernesto Portante',
+        //     'number' => '55',
+        //     'complement' => 'your_complement',
+        //     'neighborhood' => 'Jardim Maria Estela',
+        //     'city' => 'São Paulo',
+        //     'state' => 'SP',
+        //     'landmark' => 'your_landmark',
+        //     'contact_name' => 'Astralog',
+        //     'contact_phone1' => '(47) 99291-2222',
+        //     'contact_phone2' => 'contact_phone_2',
+        //     'contact_email' => 'astrolog@fretebarato.com',
+        //     'customer_service_phone' => 'customer_service_phone',
+        //     'customer_service_email' => 'customer_service_email',
+        //     'website' => 'company_website',
+        //     'title' => 'your_title',
+        //     'message' => 'your_message',
+        // ]);
 
-        $carrier3->documents()->createMany([
-            ['number' => '20588287000120'],
-            ['number' => '17000788000139'],
-        ]);
+        // $carrier3->documents()->createMany([
+        //     ['number' => '20588287000120'],
+        //     ['number' => '17000788000139'],
+        // ]);
 
 
 
